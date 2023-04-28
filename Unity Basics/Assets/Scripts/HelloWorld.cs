abe7e4c9a7e7d3ff7f2b8aab1fcb56efb4a4cc49
Dian Radoslavov <dian.radoslavov.95@gmail.com>
2023-04-29 01:03:25 +0300

    Unity Basics Demo

--- Unity Basics/Assets/Scripts/HelloWorld.cs
+++ Unity Basics/Assets/Scripts/HelloWorld.cs
@@ -5,17 +5,46 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+	public Transform sphere;
+	
+	void Awake()
+	{
+		Debug.Log("Awake");
+	}
+		
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
-        Debug.LogWarning("I SAID HELLO WORLD!");
-        Debug.LogError("YOU DIDN'T SAY HELLO!!!");
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.UpArrow))
+		{
+			this.transform.Translate(0f, 0f, 4 * Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.DownArrow))
+		{
+			this.transform.Translate(0f, 0f, 4 * -1 * Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			this.transform.Translate(4 * -1 * Time.deltaTime, 0f, 0f);
+		}
+		else if(Input.GetKey(KeyCode.RightArrow))
+		{
+			this.transform.Translate(4 * Time.deltaTime, 0f, 0f);
+		}
+		
+		if(sphere != null)
+		{
+			this.transform.LookAt(sphere);
+			if(Vector3.Distance(this.transform.position, sphere.position) > 1024 * Time.deltaTime)
+			{
+				this.transform.Translate(0f, 0f, 0.50f * Time.deltaTime);
+			}
+		}
     }
 }
